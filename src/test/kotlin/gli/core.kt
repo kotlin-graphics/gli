3a248e8709a4e2875394198048f27f386fae527f
gBarbieri <elect86@gmail.com>
2017-09-05 18:21:45 +0200

    up

--- src/test/kotlin/gli/core.kt
+++ src/test/kotlin/gli/core.kt
@@ -3,12 +3,16 @@ package gli
 import glm_.vec2.Vec2i
 import glm_.vec4.Vec4b
 import io.kotlintest.specs.StringSpec
+import java.util.*
 
 class core : StringSpec() {
 
     init {
 
         "create texture storage" {
+
+
+
 
             val texture = Texture2d(Format.RGBA8_UINT_PACK8, Vec2i(256))
             texture.clear(Vec4b())
