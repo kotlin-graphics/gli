86c6a32da63b43475411c58b713d07c271c6c784
elect <elect86@gmail.com>
2017-11-05 19:10:12 +0100

    loadImage

--- src/main/kotlin/gli_/load.kt
+++ src/main/kotlin/gli_/load.kt
@@ -27,80 +27,58 @@ interface load {
         "dds" -> gli.loadDds(path)
         "kmg" -> gli.loadKmg(path)
         "ktx" -> gli.loadKtx(path)
-//    "jpeg", "jpg", "png", "gif", "bmp", "wbmp" -> loadImage(path)
+        "jpeg", "jpg", "png", "gif", "bmp", "wbmp" -> loadImage(path)
         else -> throw Error("unsupported extension: ${path.fileName}")
     }
 
+    fun loadImage(filename: String) = loadImage(Paths.get(filename))
 
-    fun loadImage(path: String) = loadImage(Texture::class.java, path)
+    fun loadImage(uri: URI) = loadImage(Paths.get(uri))
 
-    fun loadImage(context: Class<*>, path: String) = loadImage(context.classLoader.getResource(path))
-
-    fun loadImage(url: URL) = loadImage(url.toURI())
-
-    fun loadImage(uri: URI) = loadImage(File(uri))
-
-    fun loadImage(file: File) = loadImage(ImageIO.read(file).flipY())
-
-    fun loadImage(image: BufferedImage): Texture {
-
+    /** Loads a texture storage_linear from DDS memory. Returns an empty storage_linear in case of failure.
+     *  @param uri Uri of the file to open including filaname and filename extension */
+    fun loadImage(path: Path): Texture {
+        val image = ImageIO.read(path.toFile()).flipY()
         val data = (image.raster.dataBuffer as DataBufferByte).data
-
-        val format: Format
-
-        when (image.type) {
-
-            TYPE_3BYTE_BGR -> {
-
-                format = Format.RGB8_UNORM_PACK8
-
+        val format = when (image.type) {
+            TYPE_3BYTE_BGR -> { // switch blue and red
                 repeat(image.width * image.height) {
-                    // swith blue and red
                     val tmp = data[it * 3]  // save blue
                     data[it * 3] = data[it * 3 + 2] // write red
                     data[it * 3 + 2] = tmp  // write blue
                 }
+                Format.RGB8_UNORM_PACK8
             }
             TYPE_4BYTE_ABGR -> {
-
-                format = Format.RGBA8_UNORM_PACK8
-
                 repeat(image.width * image.height) {
-                    // swith alpha and red
+                    // switch alpha and red
                     var tmp = data[it * 4]  // save alpha
                     data[it * 4] = data[it * 4 + 3] // write red
                     data[it * 4 + 3] = tmp  // write alpha
-                    // swith blue and green
+                    // switch blue and green
                     tmp = data[it * 4 + 1]  // save blue
                     data[it * 4 + 1] = data[it * 4 + 2] // write green
                     data[it * 4 + 2] = tmp  // write blue
                 }
+                Format.RGBA8_UNORM_PACK8
             }
             else -> throw Error("not yet supported")
         }
-
-        val texture = Texture(Target._2D, format, Vec3i(image.width, image.height, 1), 1, 1, 1)
-
-        repeat(texture.size) { texture.data()[it] = data[it] }
-
-        return texture
+        return Texture(Target._2D, format, Vec3i(image.width, image.height, 1), 1, 1, 1)
+                .apply { with(data()) { for (i in 0 until size) set(i, data[i]) } }
     }
 
 //fun BufferedImage.toByteBuffer() = (raster.dataBuffer as DataBufferByte).data.toByteBuffer() TODO
 
     fun BufferedImage.flipY(): BufferedImage {
-
         var scanline1: Any? = null
         var scanline2: Any? = null
-
         for (i in 0 until height / 2) {
-
             scanline1 = raster.getDataElements(0, i, width, 1, scanline1)
             scanline2 = raster.getDataElements(0, height - i - 1, width, 1, scanline2)
             raster.setDataElements(0, i, width, 1, scanline2)
             raster.setDataElements(0, height - i - 1, width, 1, scanline1)
         }
-
         return this
     }
 }
