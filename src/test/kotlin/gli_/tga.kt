11f320b680ab6106d4a2b7d70ff39034f4cac845
elect86 <elect86@gmail.com>
2018-07-29 12:12:36 +0200

    :arrow_up: glm
    Also got rid of sun.awt.image.IntegerInterleavedRaster

--- src/test/kotlin/gli_/tga.kt
+++ src/test/kotlin/gli_/tga.kt
@@ -2,7 +2,7 @@ package gli_
 
 import gli_.tga.TgaImageReaderSpi
 import io.kotlintest.specs.StringSpec
-import sun.awt.image.IntegerInterleavedRaster
+//import sun.awt.image.IntegerInterleavedRaster
 import java.awt.image.DirectColorModel
 import javax.imageio.ImageIO
 import javax.imageio.spi.IIORegistry
@@ -35,13 +35,15 @@ class tga : StringSpec() {
                 assert(alphaMask == 0)
             }
 
-            with(image.raster as IntegerInterleavedRaster) {
+//            with(image.raster as IntegerInterleavedRaster) {
+            with(image.raster) {
                 assert(width == 1024)
                 assert(height == 1024)
                 assert(numBands == 3)
                 assert(sampleModelTranslateX == 0)
                 assert(sampleModelTranslateY == 0)
-                assert(getDataOffset(0) == 0)
+//                assert(getDataOffset(0) == 0)
+                assert(dataBuffer.offset == 0)
             }
 
             assert(image.propertyNames == null)
