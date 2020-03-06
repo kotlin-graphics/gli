662d1a9b651c4f825debe40fb52855ef7e2d36ea
gbarbieri <elect86@gmail.com>
2020-03-06 16:37:46 +0100

    improved slightly gradle removed inefficient flipY method

--- src/main/kotlin/gli_/misc.kt
+++ src/main/kotlin/gli_/misc.kt
@@ -14,27 +14,9 @@ infix fun Int.has(b: Int) = (this and b) != 0
 infix fun Int.hasnt(b: Int) = (this and b) == 0
 
 fun pathOf(filename: String, vararg more: String): Path = Paths.get(filename, *more)
-fun URI.toPath(): Path = Paths.get(this)
+//fun URI.toPath(): Path = Paths.get(this)
 
 val Path.extension get() = toString().substringAfterLast(".").toLowerCase()
-
-// TODO remove
-fun flipY(buffer: ByteBuffer, width: Int, height: Int) = flipY(buffer.toByteArray(), width, height)
-
-fun flipY(buffer: ByteArray, width: Int, height: Int): ByteBuffer {
-
-    val result = BufferUtils.createByteBuffer(buffer.size)
-
-    for (scanLineIndex in 0 until height) {
-        val srcScanLineStart = width * scanLineIndex
-        val dstScanLineStart = width * (height - scanLineIndex - 1)
-        result.pos = dstScanLineStart
-        result.put(buffer, srcScanLineStart, width)
-    }
-    result.pos = 0
-
-    return result
-}
 
 fun ByteBuffer.flipY(size: Vec2i) = flipY(size.x, size.y)
 
